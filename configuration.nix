1de761a832c7b0996348e46ce125e5c5c687b931
fgtubs <f.gentzen@tu-braunschweig.de>
2026-07-27 16:56:07 +0200

    Added Thunderbird

--- configuration.nix
+++ configuration.nix
@@ -33,6 +33,9 @@
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
 
+  # Enable Thunderbird
+  programs.thunderbird.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.fin = {
     isNormalUser = true;
